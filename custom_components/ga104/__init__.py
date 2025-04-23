29ce1c310f0104b8c87f85407725fb30f8b0f1be
MoonStrider <dyf991645@gmail.com>
2025-04-23 22:31:16 +0800

    format code

--- custom_components/ga104/__init__.py
+++ custom_components/ga104/__init__.py
@@ -1,30 +1,33 @@
-#有建议可以ywyhass@126.com交流
+import logging
 import voluptuous as vol
 import homeassistant.helpers.config_validation as cv
-from homeassistant.const import (CONF_HOST,ATTR_NAME,EVENT_HOMEASSISTANT_START,EVENT_HOMEASSISTANT_STOP)
-from custom_components.ga014.ga014 import *
-#from homeassistant.core import (ServiceRegistry)
+from homeassistant.const import (CONF_HOST, EVENT_HOMEASSISTANT_START, EVENT_HOMEASSISTANT_STOP)
+from custom_components.ga104.ga014 import GA014
 
-import logging
+
 _LOGGER = logging.getLogger(__name__)
 
 # 配置文件的样式
 CONFIG_SCHEMA = vol.Schema(
     {
         "ga014": vol.Schema({vol.Required(CONF_HOST): cv.string})
-    },extra=vol.ALLOW_EXTRA)
-        
+    }, extra=vol.ALLOW_EXTRA)
+
+
 def setup(hass, config):
     conf = config['ga014']
-    hass.data['ga014']=GA014(hass,conf.get(CONF_HOST))
+    hass.data['ga014'] = GA014(hass, conf.get(CONF_HOST))
+
     def start_zinguo_update_keep_alive(event):
         hass.data['ga014'].start_keep_alive()
+
     def stop_zinguo_update_keep_alive(event):
         hass.data['ga014'].stop_keep_alive()
+
     hass.bus.listen_once(EVENT_HOMEASSISTANT_START, start_zinguo_update_keep_alive)
     hass.bus.listen_once(EVENT_HOMEASSISTANT_STOP, stop_zinguo_update_keep_alive)
-    
-    status=hass.data['ga014']._status
+
+    status = hass.data['ga014']._status
     for key in status.keys():
-        hass.helpers.discovery.load_platform('climate','ga014',{'id':key,'name':status[key]['name']}, config)
-    return True        
+        hass.helpers.discovery.load_platform('climate', 'ga014', {'id': key, 'name': status[key]['name']}, config)
+    return True
--- custom_components/ga104/climate.py
+++ custom_components/ga104/climate.py
@@ -1,35 +1,29 @@
-import json
 import time
-# import datetime
-import voluptuous as vol
-
-from homeassistant.components.climate import (ClimateEntity, PLATFORM_SCHEMA)
-from homeassistant.components.climate.const import ATTR_HVAC_MODE
-from homeassistant.components.climate.const import (HVACMode, ClimateEntityFeature)
-from homeassistant.const import (CONF_NAME, CONF_HOST, CONF_PORT, ATTR_TEMPERATURE, STATE_ON, STATE_OFF)
-from homeassistant.const import (UnitOfTemperature)
-import homeassistant.helpers.config_validation as cv
-import requests
-
 import logging
-_LOGGER = logging.getLogger(__name__)
 from datetime import timedelta
+from homeassistant.components.climate import ClimateEntity
+from homeassistant.components.climate.const import (HVACMode, ClimateEntityFeature)
+from homeassistant.const import (ATTR_TEMPERATURE, UnitOfTemperature)
+
+_LOGGER = logging.getLogger(__name__)
+
 SCAN_INTERVAL = timedelta(seconds=1)
 
-#工作模式
-MODE_HVAC={0:HVACMode.OFF,1:HVACMode.FAN_ONLY,2:HVACMode.COOL,3:HVACMode.HEAT,4:HVACMode.AUTO,5:HVACMode.DRY}
-#风力模式 #修改后可适配homekit
-# MODE_FAN ={0:'关',1:'一档风',2:'二档风', 3:'三档风', 4:'四档风', 5:'五档风', 6:'六档风', 7:'七档风',8:'自动风'}
-MODE_FAN ={0:'off',1:'low',2:'higher low', 3:'middle', 4:'really middle', 5:'medium', 6:'lower high', 7:'high',8:'auto'}
+# 工作模式
+MODE_HVAC = {0: HVACMode.OFF, 1: HVACMode.FAN_ONLY, 2: HVACMode.COOL, 3: HVACMode.HEAT, 4: HVACMode.AUTO,
+             5: HVACMode.DRY}
 
-#摇头模式 #修改后可适配homekit
-MODE_SWING={0:'off',1:'on'}
-# MODE_SWING={0:'关',1:'扫风'}
+# 风力模式
+MODE_FAN = {0: 'off', 1: 'low', 2: 'higher low', 3: 'middle', 4: 'really middle', 5: 'medium', 6: 'lower high',
+            7: 'high', 8: 'auto'}
+
+# 摇头模式
+MODE_SWING = {0: 'off', 1: 'on'}
 
 
 # pylint: disable=unused-argument
 def setup_platform(hass, config, add_devices, discovery_info=None):
-    add_devices([Thermostat(hass,discovery_info)])
+    add_devices([Thermostat(hass, discovery_info)])
 
 
 # pylint: disable=abstract-method
@@ -37,56 +31,56 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
 class Thermostat(ClimateEntity):
     """Representation of a Midea thermostat."""
 
-    def __init__(self,hass, conf):
+    def __init__(self, hass, conf):
         """Initialize the thermostat."""
         self._hass = hass
-        self._name=conf.get('name');
-        self._id=conf.get('id')
-        self._unique_id=conf.get('id')
-        self._room_temp=0
-        self._set_temp=0
-        self._run_mode=0
-        self._fan_speed=0
-        self._max_temp=30
-        self._min_temp=17
-        #电辅热
-        self._aux=False
-        #摇头模式
-        self._swing=0
-        self.time_start=0
+        self._name = conf.get('name')
+        self._id = conf.get('id')
+        self._unique_id = conf.get('id')
+        self._room_temp = 0
+        self._set_temp = 0
+        self._run_mode = 0
+        self._fan_speed = 0
+        self._max_temp = 30
+        self._min_temp = 17
+        # 电辅热
+        self._aux = False
+        # 摇头模式
+        self._swing = 0
+        self.time_start = 0
         self.update()
-        
-    #支持的模式
+
+    # 支持的模式
     @property
     def supported_features(self):
         """Return the list of supported features."""
-        return (ClimateEntityFeature.TARGET_TEMPERATURE|ClimateEntityFeature.FAN_MODE|ClimateEntityFeature.SWING_MODE|ClimateEntityFeature.AUX_HEAT)
+        return ClimateEntityFeature.TARGET_TEMPERATURE | ClimateEntityFeature.FAN_MODE | ClimateEntityFeature.SWING_MODE | ClimateEntityFeature.AUX_HEAT
 
     @property
     def should_poll(self):
         return True
 
     def update(self):
-        if time.time()-self.time_start<10:
-            return;
-        status=self._hass.data['ga014']._status[self._id]
+        if time.time() - self.time_start < 10:
+            return
+        status = self._hass.data['ga014']._status[self._id]
         # _LOGGER.error(self._name)
-        self._room_temp=float(status['room_temp'])
-        self._set_temp=float(status['cool_temp_set'])
-        self._run_mode=int(status['run_mode'])
-        self._swing=int(status['is_swing'])
-        self._aux=(int(status['is_elec_heat'])>0)
-        if int(status['is_auto_fan'])!=0:
-            self._fan_speed=8
+        self._room_temp = float(status['room_temp'])
+        self._set_temp = float(status['cool_temp_set'])
+        self._run_mode = int(status['run_mode'])
+        self._swing = int(status['is_swing'])
+        self._aux = (int(status['is_elec_heat']) > 0)
+        if int(status['is_auto_fan']) != 0:
+            self._fan_speed = 8
         else:
-            self._fan_speed=int(status['fan_speed'])
+            self._fan_speed = int(status['fan_speed'])
 
     @property
     def name(self):
         """Return the name of the thermostat."""
         return self._name
 
-    #返回unique_id以适配UI编辑
+    # 返回unique_id以适配UI编辑
     @property
     def unique_id(self):
         """Return the unique_id of the thermostat."""
@@ -107,58 +101,60 @@ class Thermostat(ClimateEntity):
         """Return the unit of measurement."""
         return UnitOfTemperature.CELSIUS
 
-    #当前室温
+    # 当前室温
     @property
     def current_temperature(self):
         return self._room_temp
 
-    #设置的温度
+    # 设置的温度
     @property
     def target_temperature(self):
         return self._set_temp
 
     def set_temperature(self, **kwargs):
-        if self._run_mode>0:    
-            self.time_start=time.time()
-            self._set_temp=kwargs.get(ATTR_TEMPERATURE)
-            self._hass.data['ga014'].set_status(self._id,self._run_mode,self._fan_speed,self._set_temp,self._aux,self._swing)
+        if self._run_mode > 0:
+            self.time_start = time.time()
+            self._set_temp = kwargs.get(ATTR_TEMPERATURE)
+            self._hass.data['ga014'].set_status(self._id, self._run_mode, self._fan_speed, self._set_temp, self._aux,
+                                                self._swing)
 
     # 直接调用climate.turn_on时的判断，bemfa平台下小爱、小度等设备开机时会直接调用turn_on，所以依照自动模式的判断逻辑
-    
     def turn_on(self, **kwargs):
-        if self._room_temp>=self._set_temp:
-            self._run_mode=2
+        if self._room_temp >= self._set_temp:
+            self._run_mode = 2
         else:
-            self._run_mode=3        
-        self._hass.data['ga014'].set_status(self._id,self._run_mode,self._fan_speed,self._set_temp,self._aux,self._swing)
+            self._run_mode = 3
+        self._hass.data['ga014'].set_status(self._id, self._run_mode, self._fan_speed, self._set_temp, self._aux,
+                                            self._swing)
 
-    #工作模式列表
+    # 工作模式列表
     @property
     def hvac_modes(self):
         return list(MODE_HVAC.values())
 
-    #工作模式
+    # 工作模式
     @property
     def hvac_mode(self):
         return MODE_HVAC[self._run_mode]
 
     def set_hvac_mode(self, hvac):
-        self._run_mode=list(MODE_HVAC.keys())[list(MODE_HVAC.values()).index(hvac)]
+        self._run_mode = list(MODE_HVAC.keys())[list(MODE_HVAC.values()).index(hvac)]
         # 处理空调没有自动模式的问题，有时Siri开空调会选择自动模式
-        if self._run_mode==4:
-            if self._room_temp>=self._set_temp:
-                self._run_mode=2
+        if self._run_mode == 4:
+            if self._room_temp >= self._set_temp:
+                self._run_mode = 2
             else:
-                self._run_mode=3
+                self._run_mode = 3
 
-        self._hass.data['ga014'].set_status(self._id,self._run_mode,self._fan_speed,self._set_temp,self._aux,self._swing)
-        
-    #风力模式
+        self._hass.data['ga014'].set_status(self._id, self._run_mode, self._fan_speed, self._set_temp, self._aux,
+                                            self._swing)
+
+    # 风力模式
     @property
     def fan_modes(self):
         """Return the list of available fan modes."""
         return list(MODE_FAN.values())
-    
+
     @property
     def fan_mode(self):
         """Return the fan setting."""
@@ -166,46 +162,46 @@ class Thermostat(ClimateEntity):
 
     def set_fan_mode(self, fan):
         # month=datetime.datetime.today().month
-        if self._run_mode>0:
-            self.time_start=time.time()
-            self._fan_speed=list(MODE_FAN.keys())[list(MODE_FAN.values()).index(fan)]
+        if self._run_mode > 0:
+            self.time_start = time.time()
+            self._fan_speed = list(MODE_FAN.keys())[list(MODE_FAN.values()).index(fan)]
             # 处理Siri无法关闭空调问题，Siri只能将空调风速调为关闭，不能关闭空调本体
-            if self._fan_speed==0:
-                self._run_mode=0
-            
-            self._hass.data['ga014'].set_status(self._id,self._run_mode,self._fan_speed,self._set_temp,self._aux,self._swing)
+            if self._fan_speed == 0:
+                self._run_mode = 0
+
+            self._hass.data['ga014'].set_status(self._id, self._run_mode, self._fan_speed, self._set_temp, self._aux,
+                                                self._swing)
 
         # 处理Siri无法开启空调问题，有时Siri只会调整空调风速，不会开启空调本体
-        if self._run_mode==0:
-            self._fan_speed=list(MODE_FAN.keys())[list(MODE_FAN.values()).index(fan)]
-            if self._fan_speed>0:
-                self.time_start=time.time()
-                if self._room_temp>=self._set_temp:
-                    self._run_mode=2
+        if self._run_mode == 0:
+            self._fan_speed = list(MODE_FAN.keys())[list(MODE_FAN.values()).index(fan)]
+            if self._fan_speed > 0:
+                self.time_start = time.time()
+                if self._room_temp >= self._set_temp:
+                    self._run_mode = 2
                 else:
-                    self._run_mode=3
-            self._hass.data['ga014'].set_status(self._id,self._run_mode,self._fan_speed,self._set_temp,self._aux,self._swing)
-
-        
-
-
+                    self._run_mode = 3
+            self._hass.data['ga014'].set_status(self._id, self._run_mode, self._fan_speed, self._set_temp, self._aux,
+                                                self._swing)
 
     @property
     def is_aux_heat(self):
         return self._aux
-        
+
     def turn_aux_heat_on(self):
-        if self._run_mode>0:
-            self.time_start=time.time()
+        if self._run_mode > 0:
+            self.time_start = time.time()
             self._aux = True
-            self._hass.data['ga014'].set_status(self._id,self._run_mode,self._fan_speed,self._set_temp,self._aux,self._swing)
+            self._hass.data['ga014'].set_status(self._id, self._run_mode, self._fan_speed, self._set_temp, self._aux,
+                                                self._swing)
 
     def turn_aux_heat_off(self):
-        if self._run_mode>0:
-            self.time_start=time.time()
+        if self._run_mode > 0:
+            self.time_start = time.time()
             self._aux = False
-            self._hass.data['ga014'].set_status(self._id,self._run_mode,self._fan_speed,self._set_temp,self._aux,self._swing)
-        
+            self._hass.data['ga014'].set_status(self._id, self._run_mode, self._fan_speed, self._set_temp, self._aux,
+                                                self._swing)
+
     @property
     def swing_modes(self):
         return list(MODE_SWING.values())
@@ -215,8 +211,8 @@ class Thermostat(ClimateEntity):
         return MODE_SWING[self._swing]
 
     def set_swing_mode(self, swing_mode):
-        if self._run_mode>0:
-            self.time_start=time.time()
-            self._swing=list(MODE_SWING.keys())[list(MODE_SWING.values()).index(swing_mode)]
-            self._hass.data['ga014'].set_status(self._id,self._run_mode,self._fan_speed,self._set_temp,self._aux,self._swing)
-    
+        if self._run_mode > 0:
+            self.time_start = time.time()
+            self._swing = list(MODE_SWING.keys())[list(MODE_SWING.values()).index(swing_mode)]
+            self._hass.data['ga014'].set_status(self._id, self._run_mode, self._fan_speed, self._set_temp, self._aux,
+                                                self._swing)
